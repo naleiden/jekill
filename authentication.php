03f4375da8460c82037aa2ad05162ce235ae7c6e
Nicholas Leidenfrost <nicholas@Nicholass-MacBook-Pro.local>
2015-06-17 08:49:01 -0500

    Allow authentication to return a HTTP 401 error as opposed to redirecting (for unauth'd ajax calls)

--- authentication.php
+++ authentication.php
@@ -7,7 +7,11 @@ session_start();
 // echo $_SESSION["{$LOGIN_ID}_user_ID"] . " vs " . $_SESSION["{$LOGIN_ID}_permissions"];
 
 if (/* $_SERVER['HTTPS'] != 'on' || */  $_SESSION["{$LOGIN_ID}_user_ID"] == "" || $_SESSION["{$LOGIN_ID}_permissions"] == "" || $_SESSION["{$LOGIN_ID}_permissions"] == 0) {
-	header("Location: admin_login.php");
+	if (isset($request_type) && $request_type == "ajax") {
+		header("HTTP/1.1 401 Unauthorized");
+	} else {
+		header("Location: admin_login.php");
+	}
 	exit;
 }
 
